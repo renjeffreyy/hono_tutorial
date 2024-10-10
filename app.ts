48a6307dfa785fabee4b6f38109479d4eed80b1c
Jeffrey Ren <renjeffreyy@gmail.com>
2024-10-10 15:55:19 -0700

    added routes to hono server

--- app.ts
+++ app.ts
@@ -1,10 +1,16 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+import { expensesRoute } from './routes/expenses'
 
 const app = new Hono()
+
+app.use(logger())
+app.route("/",expensesRoute)
 
 app.get("/test", c => {
     return c.json({"message": "testing!"})
 })
+
 
 
 export default app
